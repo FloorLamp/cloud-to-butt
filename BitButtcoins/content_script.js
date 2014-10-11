d3596261526b21306e5f61f907967d0427e97723
Norton Wang <nortantivirus@gmail.com>
2014-10-11 15:15:32 -0400

    allow partials, btc/BTC, dogecoin

--- BitButtcoins/content_script.js
+++ BitButtcoins/content_script.js
@@ -1,45 +1,43 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+  var child, next;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+  switch ( node.nodeType )
+  {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child )
+      {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
-	var v = textNode.nodeValue;
-	v = v.replace(/\bBitcoin\b/g, "Buttcoin");
-	v = v.replace(/\bbitcoin\b/g, "buttcoin");
-    v = v.replace(/\bLitecoin\b/g, "LiteButtcoin");
-    v = v.replace(/\blitecoin\b/g, "liteButtcoin");
-    v = v.replace(/\bPeercoin\b/g, "PeerButtcoin");
-    v = v.replace(/\bpeercoin\b/g, "peerButtcoin");
-	//v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	//v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+  var v = textNode.nodeValue;
+  v = v.replace(/Bitcoin/g, "Buttcoin");
+  v = v.replace(/bitcoin/g, "buttcoin");
+  v = v.replace(/BTC/g, "BUTT");
+  v = v.replace(/btc/g, "butt");
+  v = v.replace(/Litecoin/g, "LiteButtcoin");
+  v = v.replace(/litecoin/g, "liteButtcoin");
+  v = v.replace(/Dogecoin/g, "DogeButtcoin");
+  v = v.replace(/dogecoin/g, "dogeButtcoin");
+
+  textNode.nodeValue = v;
 }
-
-
